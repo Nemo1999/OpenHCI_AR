fd061532ce2730951b80a4f0baaf403aed71d34c
a0716116z <65119444+a0716116z@users.noreply.github.com>
2022-08-28 10:59:12 +0800

    Fix Text

--- index.js
+++ index.js
@@ -17,30 +17,25 @@ function startTimer(duration, display) {
         }
     }, 1000);
 }
-window.onload = function () {
-    const gamestart = document
-      .querySelector(".gameStart")
-      .addEventListener("click", function () { 
-         display = document.querySelector('#timeleft');  
-    startTimer(leftsec, display);
-      document.getElementById("gameStart").classList.add("displaynone")
-       document.getElementById('player').load();
-       document.getElementById('player').play();
-      });
-    AFRAME.registerComponent('markerhandler', {
+AFRAME.registerComponent('markerhandler', {
     init: function () {
         this.el.sceneEl.addEventListener('markerFound', () => {
         console.log("Marker found");
         const count_display = document.querySelector('#countleft');  
-        countleft-=1;
+        countleft=countleft -1;
         count_display.textContent = countleft;
-
-        // startTimer(leftsec, display);
-        // redirect to custom URL e.g. google.com
-        // window.location = 'https://www.google.com/';
       })
     }
   });
-
+window.onload = function () {
+    const gamestart = document
+      .querySelector(".gameStart")
+      .addEventListener("click", function () { 
+      display = document.querySelector('#timeleft');  
+      startTimer(leftsec, display);
+      document.getElementById("gameStart").classList.add("displaynone")
+       document.getElementById('player').load();
+       document.getElementById('player').play();
+      });
 };
 
